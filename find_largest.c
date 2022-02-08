ed361e4b7bb05924f316e0a31e6899cfb8b2acce
Noah <69133832+nkr38@users.noreply.github.com>
2022-02-08 13:29:37 -0500

    Add files via upload

---
+++ find_largest.c
@@ -0,0 +1,43 @@
+/* Homework 3: Question 2
+   Compile with: gcc -std=c89 -o Q2 Q2.c
+   Then run your compiled program with: ./Q2
+*/
+
+#include <stdio.h>
+
+int *find_largest (int *a, int n)
+{
+  int i, index, large = 0;
+  int* var;
+  
+  for(i=0; i < n; i++)
+  {
+    if(a[i] > large)
+    {
+      large = a[i];
+      index = i;
+    }
+  }
+  var = a + index;
+  return var;
+}
+
+
+int main ()
+{
+  int *largest;
+  
+  int numbers[] = {10,  1, 54,  8, 78, 13,  4,
+                   32, 72, 13, 43, 66, 42, 21,
+                   41, 87, 23, 11, 52,  7,  2,
+                    3, 33,  5,  9, 67, 12, 17};
+
+  int n = sizeof(numbers)/sizeof(int);
+  
+  largest = find_largest(numbers, n);
+  
+  printf("Of the %d numbers passed,\n", n);
+  printf("%d was the largest\n", *largest);
+  
+  return 0;
+}
