9ec1291ac887ddcad1b2b73f61dd54556cb734b7
Noah <69133832+nkr38@users.noreply.github.com>
2022-02-08 14:20:37 -0500

    Update zero_array.c

--- zero_array.c
+++ zero_array.c
@@ -1,6 +1,8 @@
 /* Homework 3: Question 2
    Compile with: gcc -std=c89 -o Q2 Q2.c
    Then run your compiled program with: ./Q2
+   
+   Takes an array (numbers[]) filled with ints and finds which number was the largest and how many numbers were given originally.
 */
 
 #include <stdio.h>
