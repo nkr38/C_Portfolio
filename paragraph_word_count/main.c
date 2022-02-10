3ea559e1493696bfccc1d2dc03d91e6961131b5b
Noah <69133832+nkr38@users.noreply.github.com>
2022-02-10 10:33:58 -0500

    Add files via upload

---
+++ paragraph_word_count/main.c
@@ -0,0 +1,43 @@
+/* 
+    Noah R.
+    This code creates a text file that looks at another file and stores each paragraph number and how many words are in that paragraph.  
+*/
+#include <stdio.h>
+
+int main()
+{
+  /* Your code goes here! */
+  FILE *fp;
+  FILE *counter;
+  int c, i=0, j, k, store[16];
+
+  fp = fopen("lorum.txt", "r");
+  counter = fopen("count.txt", "w");
+  
+  for(j = 0; j < 16; j++)
+  {
+    store[j]=0;
+  }
+  
+  while ((c = fgetc(fp)) != EOF)
+  {
+      if (c != '\n')
+      {
+        store[i]++;
+      }
+      else
+      {
+        i++;
+      }  
+  }
+
+  for(k = 1; k < 17; k++)
+  {
+    fprintf(counter, "%d: %d\n", k, store[k-1]);
+  }
+  
+  fclose(fp);
+  fclose(counter);
+
+  return 0;
+}
